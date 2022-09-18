120f8ac5ad4f7912bb303250ecb7aee60e9a62ca
hyungkiShin <tkaqkeldk@nate.com>
2022-09-19 00:27:49 +0900

    [feat] post Head 추가 및, font-awesome 추가
    yarn add @fortawesome/fontawesome-svg-core @fortawesome/free-solid-svg-icons @fortawesome/react-fontawesome

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,37 +1,45 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import { PostPageItemType } from 'types/PostItem.types' // 바로 아래에서 정의할 것입니다
 import Template from 'components/common/Template'
+import PostHead from 'components/post/PostHead'
 
-// eslint-disable-next-line @typescript-eslint/ban-types
-type PostTemplateProps = {}
-
-const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  console.log(props)
-
-  return <Template>Post Template</Template>
-}
-
-export default PostTemplate
-
-export const queryMarkdownDataBySlug = graphql`
-  query queryMarkdownDataBySlug($slug: String) {
-    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
-      edges {
-        node {
-          html
-          frontmatter {
-            title
-            summary
-            date(formatString: "YYYY.MM.DD.")
-            categories
-            thumbnail {
-              childImageSharp {
-                gatsbyImageData
-              }
-            }
-          }
-        }
-      }
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[] // 존재하지 않는 타입이므로 에러가 발생하지만 일단 작성해주세요
     }
   }
-`
+}
+
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary, // 나중에 사용할 예정입니다!
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+        },
+      },
+    },
+  } = edges[0]
+
+  return (
+    <Template>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+    </Template>
+  )
+}
