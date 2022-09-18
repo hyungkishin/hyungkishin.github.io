1cb91bd0949ee200428fdd91025a0b8b7b5328be
hyungkiShin <tkaqkeldk@nate.com>
2022-09-18 18:21:19 +0900

    [feat] blog image add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
+import styled from '@emotion/styled'
+import GlobalStyle from 'components/common/GlobalStyle'
+import ProfileImage from 'components/main/ProfileImage'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`
 
 const IndexPage: FunctionComponent = function () {
-  return <Text text="Home" />
+  return (
+    <Container>
+      <GlobalStyle />
+      <ProfileImage />
+    </Container>
+  )
 }
 
 export default IndexPage
